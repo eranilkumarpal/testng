becdf5009de85dac95d9269646c2b1de299093d4
Ashish <kumar.ashish@salesforce.com>
2019-04-28 16:49:13 +0530

    Added sub test

--- src/test/java/demodebug/demodebug/DemoMathTest.java
+++ src/test/java/demodebug/demodebug/DemoMathTest.java
@@ -23,6 +23,16 @@ public class DemoMathTest {
 		int actual = demoMath.add(n1, n2);
 		Assert.assertEquals(actual, expected);
 	}
-   
+   @Test
+        public void testSub()
+        {
+                int n1 = 10;
+                int n2 = 5;
+                int expected = 5;
+                DemoMath demoMath = new DemoMath();
+                int actual = demoMath.sub(n1, n2);
+                Assert.assertEquals(actual, expected);
+        }
+
 	
 }
