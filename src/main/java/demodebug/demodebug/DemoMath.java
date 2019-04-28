1edc4428ac98a932542a5e4e0488c4d279439f81
anil kumar pal <anilkumar.pal@gmail.com>
2019-04-28 16:41:09 +0530

    firstcommit

---
+++ src/main/java/demodebug/demodebug/DemoMath.java
@@ -0,0 +1,19 @@
+package demodebug.demodebug;
+
+public class DemoMath {
+	
+	public int add(int n1, int n2)
+	{
+		System.out.println("Enter demo math add method");
+		return n1+n2+1;
+		
+		
+	}
+
+	public int sub(int n1, int n2)
+	{
+		System.out.println("Enter demo math sub method");
+		return n1-n2;
+		
+	}
+}
---
+++ src/test/java/demodebug/demodebug/DemoMathTest.java
@@ -0,0 +1,28 @@
+package demodebug.demodebug;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class DemoMathTest {
+	
+	
+	@BeforeClass
+	public void setup()
+	{
+		
+		System.out.println("Enter into setup");
+	}
+	@Test
+	public void testAdd()
+	{
+		int n1 = 10;
+		int n2 = 5;
+		int expected = 15;
+		DemoMath demoMath = new DemoMath();
+		int actual = demoMath.add(n1, n2);
+		Assert.assertEquals(actual, expected);
+	}
+   
+	
+}
